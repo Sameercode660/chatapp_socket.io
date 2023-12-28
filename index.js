9a760a0d42b5767bba8af4491fc8889a8fcbc1c5
Mohd Sameer <mesh789736@gmail.com>
2023-12-27 22:14:16 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path'); // Add the 'path' module
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+// Serve the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html')); // Use path.join for file paths
+});
+
+io.on('connection', (socket) => {
+  console.log('New user is connected', socket.id);
+  socket.on('chat-message', (msg) => {
+    io.emit('chat-message', msg)
+})
+});
+
+server.listen(8080, () => {
+  console.log('Server is listening on port 8080');
+  console.log(io);
+});
